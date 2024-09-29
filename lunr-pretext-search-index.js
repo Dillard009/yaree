e305fd61bef054b633efb48100001046b209c90a
Dillard009 <eduardok009@outlook.com>
2024-09-29 19:11:16 -0300

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,239 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "meta_frontmatter-2",
+  "level": "1",
+  "url": "meta_frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " my-website.org  copyright  "
+},
+{
+  "id": "meta_frontmatter-3",
+  "level": "1",
+  "url": "meta_frontmatter-3.html",
+  "type": "Dedication",
+  "number": "",
+  "title": "Dedication",
+  "body": " For ...  "
+},
+{
+  "id": "meta_frontmatter-4",
+  "level": "1",
+  "url": "meta_frontmatter-4.html",
+  "type": "Acknowledgements",
+  "number": "",
+  "title": "Acknowledgements",
+  "body": " I would like to thank...  "
+},
+{
+  "id": "meta_frontmatter-preface",
+  "level": "1",
+  "url": "meta_frontmatter-preface.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Preface",
+  "body": "   About this book:  "
+},
+{
+  "id": "meta_frontmatter-6",
+  "level": "1",
+  "url": "meta_frontmatter-6.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Not just that..",
+  "body": " Not just that..  I still have more to say!  "
+},
+{
+  "id": "sec_first-intro",
+  "level": "1",
+  "url": "sec_first-intro.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Introduction to First Chapter",
+  "body": " Introduction to First Chapter   Intro to section, because we will have subsections.  Another paragraph of the introduction.    A subsection  Content of the subsection.  More content.    Another Subsection  Text with some math: only for some values of .    After the last subsection, you might have a conclusion.   "
+},
+{
+  "id": "sec_first-examples",
+  "level": "1",
+  "url": "sec_first-examples.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Examples",
+  "body": " Examples  Here is an example:    The statement of the example, since it will have a solution. What is the solution?    This is the solution.    Here is another example, but without a solution   This is an example of an example without a solution (or hint or answer) so the paragraph doesn't need to be inside a statement tag.   Although the next things are not examples, they give examples of blocks that are not examples. First a theorem.   Theorem Title   This is the statement of the theorem.    And this is its proof.      A corollary that doesn't need a proof.    Perhaps at the end of the section, you want to add a note to the reader about how what you have fits into the larger scope of mathematics.   Larger Context  Comment to the reader here.   Of course, if you want more information, you might see .  "
+},
+{
+  "id": "sec_first-examples-3",
+  "level": "2",
+  "url": "sec_first-examples.html#sec_first-examples-3",
+  "type": "Example",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  The statement of the example, since it will have a solution. What is the solution?    This is the solution.   "
+},
+{
+  "id": "sec_first-examples-5",
+  "level": "2",
+  "url": "sec_first-examples.html#sec_first-examples-5",
+  "type": "Example",
+  "number": "1.2.2",
+  "title": "",
+  "body": " This is an example of an example without a solution (or hint or answer) so the paragraph doesn't need to be inside a statement tag.  "
+},
+{
+  "id": "thm_first-examples-theorem",
+  "level": "2",
+  "url": "sec_first-examples.html#thm_first-examples-theorem",
+  "type": "Theorem",
+  "number": "1.2.3",
+  "title": "Theorem Title.",
+  "body": " Theorem Title   This is the statement of the theorem.    And this is its proof.   "
+},
+{
+  "id": "cor_first-examples-corollary",
+  "level": "2",
+  "url": "sec_first-examples.html#cor_first-examples-corollary",
+  "type": "Corollary",
+  "number": "1.2.4",
+  "title": "",
+  "body": "  A corollary that doesn't need a proof.   "
+},
+{
+  "id": "remark_first-examples-context",
+  "level": "2",
+  "url": "sec_first-examples.html#remark_first-examples-context",
+  "type": "Remark",
+  "number": "1.2.5",
+  "title": "Larger Context.",
+  "body": " Larger Context  Comment to the reader here.  "
+},
+{
+  "id": "ex_first",
+  "level": "1",
+  "url": "ex_first.html",
+  "type": "Exercises",
+  "number": "1.3",
+  "title": "My Exercises",
+  "body": "  My Exercises   Is this an exercise?   Yes.   It is, but not a very good one.    Another, with a hint.   The hint.      "
+},
+{
+  "id": "ex_first-2",
+  "level": "2",
+  "url": "ex_first.html#ex_first-2",
+  "type": "Exercise",
+  "number": "1.3.1",
+  "title": "",
+  "body": " Is this an exercise?   Yes.   It is, but not a very good one.  "
+},
+{
+  "id": "ex_first-3",
+  "level": "2",
+  "url": "ex_first.html#ex_first-3",
+  "type": "Exercise",
+  "number": "1.3.2",
+  "title": "",
+  "body": " Another, with a hint.   The hint.  "
+},
+{
+  "id": "ch_empty",
+  "level": "1",
+  "url": "ch_empty.html",
+  "type": "Chapter",
+  "number": "2",
+  "title": "(No title)",
+  "body": " (No title)   Try adding your own content here!   "
+},
+{
+  "id": "sec_features-blocks",
+  "level": "1",
+  "url": "sec_features-blocks.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Environments and Blocks",
+  "body": " Environments and Blocks  Some text   My Theorem   Theorem statement.    Proof of theorem.      Statement of example    The solution.    Now a figure.   A frog   A nice-looking frog with a longish description.    "
+},
+{
+  "id": "thm_features-blocks-my-theorem",
+  "level": "2",
+  "url": "sec_features-blocks.html#thm_features-blocks-my-theorem",
+  "type": "Theorem",
+  "number": "3.1.1",
+  "title": "My Theorem.",
+  "body": " My Theorem   Theorem statement.    Proof of theorem.   "
+},
+{
+  "id": "sec_features-blocks-4",
+  "level": "2",
+  "url": "sec_features-blocks.html#sec_features-blocks-4",
+  "type": "Example",
+  "number": "3.1.2",
+  "title": "",
+  "body": "  Statement of example    The solution.   "
+},
+{
+  "id": "sec_features-blocks-6",
+  "level": "2",
+  "url": "sec_features-blocks.html#sec_features-blocks-6",
+  "type": "Figure",
+  "number": "3.1.3",
+  "title": "",
+  "body": " A frog   A nice-looking frog with a longish description.   "
+},
+{
+  "id": "meta_backmatter-2",
+  "level": "1",
+  "url": "meta_backmatter-2.html",
+  "type": "Appendix",
+  "number": "A",
+  "title": "Selected Hints",
+  "body": " Selected Hints  "
+},
+{
+  "id": "meta_backmatter-3",
+  "level": "1",
+  "url": "meta_backmatter-3.html",
+  "type": "Appendix",
+  "number": "B",
+  "title": "Selected Solutions",
+  "body": " Selected Solutions  "
+},
+{
+  "id": "meta_backmatter-4",
+  "level": "1",
+  "url": "meta_backmatter-4.html",
+  "type": "Appendix",
+  "number": "C",
+  "title": "List of Symbols",
+  "body": " List of Symbols   "
+},
+{
+  "id": "meta_backmatter-5",
+  "level": "1",
+  "url": "meta_backmatter-5.html",
+  "type": "Index",
+  "number": "",
+  "title": "Index",
+  "body": " Index   "
+},
+{
+  "id": "meta_backmatter-6",
+  "level": "1",
+  "url": "meta_backmatter-6.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
